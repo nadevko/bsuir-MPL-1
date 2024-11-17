389b7428e999b67a7d4006bb4068c983d26a05ee
Nadeŭka <nadevko@riseup.net>
2024-11-18 00:10:32 +0300

    baradzin init

---
+++ baradzin/Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version: 5.8
+import PackageDescription
+
+let package = Package(
+  name: "MPL1",
+  dependencies: [
+    // .package(path: "./02"),
+    // .package(path: "./03"),
+    // .package(path: "./04"),
+  ]
+)
